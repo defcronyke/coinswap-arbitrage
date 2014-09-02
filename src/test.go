49dc441d553058dc01caa2d1a45315788da6ed11
Jeremy Carter <Jeremy@JeremyCarter.ca>
2014-09-02 16:20:05 -0400

    Deal with different json response types.
    So far only handles a maps with string keys, and
    arrays.

--- src/test.go
+++ src/test.go
@@ -18,24 +18,9 @@ func get_content() {
     }
     client := &http.Client{Transport: tr}
     // api url that responds with json data
-    url := "https://api.coin-swap.net/market/stats/DOGE/BTC"
-    //url := "https://api.coin-swap.net/market/summary"
+    //url := "https://api.coin-swap.net/market/stats/DOGE/BTC"
+    url := "https://api.coin-swap.net/market/summary"
     
-
-
-//    type marketStats struct {
-//        Marketid string `json:"marketid,omitempty"`
-//        Symbol string `json:"symbol,omitempty"`
-//        Exchange string `json:"exchange,omitempty"`
-//        Lastprice string `json:"lastprice,omitempty"`
-//        Dayvolume string `json:"dayvolume,omitempty"`
-//        Dayhigh string `json:"dayhigh,omitempty"`
-//        Daylow string `json:"daylow,omitempty"`
-//        Ask string `json:"ask,omitempty"`
-//        Bid string `json:"bid,omitempty"`
-//        Openorders string `json:"openorders,omitempty"`
-//    }
-
 	var marketStats interface{}
 
 
@@ -55,17 +40,28 @@ func get_content() {
         fmt.Printf("%s",apiError)
     }
 
-    //var jsonData marketStats
     err := json.Unmarshal(apiResponse, &marketStats)
     if err != nil {
         fmt.Printf("Error: %v\n", err)
     }
-    // Print json data to screen
     
-    stats_map := marketStats.(map[string]interface{})
-    fmt.Printf("Results: %v\n", stats_map["marketid"])
-
+    // Print json data to screen
+    switch marketStats.(type) {
+        case map[string]interface{}:	// If json response type is a map with string keys.
+        	stats := marketStats.(map[string]interface{})
+    		fmt.Printf("Results: %v\n", stats["marketid"])
+    	case []interface{}:				// if type is an array.
+    		stats := marketStats.([]interface{})
+    		
+    		fmt.Printf("Results: ")
+    		for _,v := range stats {
+    			fmt.Printf("\n%s", v)    
+    		}
+    		
     }
+
+
+}
 
 func main() {
     get_content()
