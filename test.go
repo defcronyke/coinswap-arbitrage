f6c08258c6a35e452bd79ffc5a61351fed969858
ianchildress <ianchildress@gmail.com>
2014-09-03 15:18:02 -0400

    Update

--- test.go
+++ test.go
@@ -8,12 +8,12 @@ import (
 
 func main() {
     coinswap_data := Get_coinswap()
-    //poloniex_data := poloniex.Get_content()
-    //bittrex_data := bittrex.Get_content()
-    //mintpal_data := mintpal.Get_content()
+    poloniex_data := poloniex.Get_content()
+    bittrex_data := bittrex.Get_content()
+    mintpal_data := mintpal.Get_content()
 
     fmt.Printf("Coin-Swap: %v\n", coinswap_data)
-    //fmt.Printf("Poloniex: %v\n", poloniex_data)
-    //fmt.Printf("Bittrex: %v\n", bittrex_data)
-    //fmt.Printf("Mintpal: %v\n", mintpal_data)
+    fmt.Printf("Poloniex: %v\n", poloniex_data)
+    fmt.Printf("Bittrex: %v\n", bittrex_data)
+    fmt.Printf("Mintpal: %v\n", mintpal_data)
 }
