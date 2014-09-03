d1f530a0f6304f006c25a98d76b25686cab664cc
ianchildress <ianchildress@gmail.com>
2014-09-03 15:48:11 -0400

    update

--- test.go
+++ test.go
@@ -11,9 +11,21 @@ func main() {
     poloniex_data := Get_poloniex()
     bittrex_data := Get_bittrex()
     mintpal_data := Get_mintpal()
-
+    
+    test := Compare(coinswap_data,mintpal_data)
+    
+/*
     fmt.Printf("Coin-Swap: %v\n", coinswap_data)
     fmt.Printf("Poloniex: %v\n", poloniex_data)
     fmt.Printf("Bittrex: %v\n", bittrex_data)
     fmt.Printf("Mintpal: %v\n", mintpal_data)
+*/
+}
+
+func Compare(coinswap,exchange map[string]float64) int {
+    for market,price := range coinswap {
+        if market,ok := exchange[market]; ok {
+            fmt.Printf("Matching market: %v\n", coinswap_data)
+        }
+    }
 }
