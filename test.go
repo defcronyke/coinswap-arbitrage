de1c9147473b464772d00705c67c205a17f4605b
ianchildress <ianchildress@gmail.com>
2014-09-03 16:01:10 -0400

    a

--- test.go
+++ test.go
@@ -31,6 +31,11 @@ func Compare(coinswap,exchange map[string]float64) int {
     for market,price := range coinswap {
         if market,ok := exchange[market]; ok {
             fmt.Printf("Matching market: %v\n", market)
+            if price < exchange[market] {
+                fmt.Printf("Arbitrage available: %v\n", market)
+            }
+        } else {
+            fmt.Printf("No match: %v\n", market)
         }
     }
     return 1;
